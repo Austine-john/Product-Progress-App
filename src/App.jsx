f0086eb5871cef06b0efd3b91fb07ee1c423cd59
Austine-john <ochiengaustine582@gmail.com>
2025-08-29 09:24:27 +0300

    navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { DataProvider } from "./context/DataContext";
-import Navbar from "./Components/Navbar";
-import Dashboard from "./Pages/Dashboard";
-import ProductList from "./Pages/ProductList";
-import ProductDetails from "./Pages/ProductDetails";
-import NewRequest from "./Pages/NewRequest";
-import Reports from "./Pages/Reports";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
-export default function App() {
-  return (
-    <DataProvider>
-      <Router>
-        <Navbar />
-        <div className="container mt-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/products/:id" element={<ProductDetails />} />
-            <Route path="/new-request" element={<NewRequest />} />
-            <Route path="/reports" element={<Reports />} />
-          </Routes>
-        </div>
-      </Router>
-    </DataProvider>
-  );
+export default function App(){
+
+  return(
+<>
+    <Dashboard />
+</>
+  )
 }
---
+++ src/Components/Common/SearchBar.jsx
@@ -0,0 +1,9 @@
+export default function SearchBar(){
+
+    return(
+        <div className="search-bar">
+            <input type="text" placeholder="Search products ..." />
+
+        </div>
+    )
+}
